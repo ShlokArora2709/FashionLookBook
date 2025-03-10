5cfb0cfeb361985ed13378fdbbfc9ed0bcec44f2
Shlok_Arora <shlokarora2709@gmail.com>
2025-03-10 11:27:22 +0000

    1

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,101 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(FashionLookbookApp());
+}
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class FashionLookbookApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: 'Fashion Lookbook',
+      theme: ThemeData(primarySwatch: Colors.pink),
+      home: FashionSearchScreen(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class FashionSearchScreen extends StatefulWidget {
+  @override
+  _FashionSearchScreenState createState() => _FashionSearchScreenState();
+}
+
+class _FashionSearchScreenState extends State<FashionSearchScreen> {
+  TextEditingController _searchController = TextEditingController();
+  List<String> _imageUrls = [];
+  bool _isLoading = false;
+
+  Future<void> fetchFashionImages(String query) async {
+    setState(() => _isLoading = true);
+
+    const String apiKey =
+        'AIzaSyBpKVG6btdbU5WbWax4SHO6OXwcKf6hUAg'; // Replace securely
+    const String cx = '308a8669e24444efb'; // Replace securely
+
+    final String url =
+        'https://www.googleapis.com/customsearch/v1?q=$query&cx=$cx&key=$apiKey&searchType=image';
+
+    try {
+      final response = await http.get(Uri.parse(url));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        List<String> imageUrls = List<String>.from(
+            data['items'].map((item) => item['link'].toString()));
+        setState(() => _imageUrls = imageUrls);
+      } else {
+        throw Exception('Failed to load images');
+      }
+    } catch (e) {
+      print('Error: $e');
+    } finally {
+      setState(() => _isLoading = false);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      appBar: AppBar(title: Text('Fashion Lookbook')),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                hintText: 'Search fashion style...',
+                border: OutlineInputBorder(),
+                suffixIcon: IconButton(
+                  icon: Icon(Icons.search),
+                  onPressed: () {
+                    if (_searchController.text.isNotEmpty) {
+                      fetchFashionImages(_searchController.text);
+                    }
+                  },
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            _isLoading
+                ? CircularProgressIndicator()
+                : Expanded(
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 8,
+                        mainAxisSpacing: 8,
+                      ),
+                      itemCount: _imageUrls.length,
+                      itemBuilder: (context, index) {
+                        return Image.network(_imageUrls[index],
+                            fit: BoxFit.cover);
+                      },
+                    ),
+                  ),
+          ],
         ),
       ),
     );
